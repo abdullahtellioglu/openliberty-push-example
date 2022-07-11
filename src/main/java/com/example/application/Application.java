3dd07e06813202de464422e390e09ceb4aa444a9
abdullahtellioglu <abdullah@vaadin.com>
2022-07-11 16:18:52 +0300

    initial commits

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,10 +2,14 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.shared.communication.PushMode;
+import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -19,10 +23,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @Theme(value = "mytodo")
 @PWA(name = "My Todo", shortName = "My Todo", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
+@Push(value = PushMode.AUTOMATIC, transport = Transport.LONG_POLLING)
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
-
 }
